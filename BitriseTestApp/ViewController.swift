22ce0a4f42c7f211692e96bd85c8feaec216e47a
div-antk <51283468+owaii@users.noreply.github.com>
2021-08-25 22:05:03 +0900

    Initial Commit

---
+++ BitriseTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitriseTestApp
+//
+//  Created by Takuya Ando on 2021/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
